b5cd5c44cad6ddf7b14212c8132b08b208c17577
Jorge Flores <jafp07@gmail.com>
2022-06-06 18:57:44 -0500

    added initializr

---
+++ src/main/java/com/example/dsiiiarchive/DsiiiArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsiiiarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsiiiArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsiiiArchiveApplication.class, args);
+	}
+
+}
