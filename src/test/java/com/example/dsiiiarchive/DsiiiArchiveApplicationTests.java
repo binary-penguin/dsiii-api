5b5ff0d764c0bc902074986447546d2e18461351
Jorge Flores <jafp07@gmail.com>
2022-06-11 18:59:00 -0500

    added new project

---
+++ src/test/java/com/example/dsiiiarchive/DsiiiArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsiiiarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsiiiArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
